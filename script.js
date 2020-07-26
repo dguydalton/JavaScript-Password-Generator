053d63d3840a4e21dd5c8750b7ef4f74b097e1b9
Jarryd Sterlingstreet Derek <sterlingstreet@Dereks-Mac.local>
2020-07-26 15:51:36 -0400

    javascript variables

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
-const characterAmountRange = document.getElementById
-('characterAmountRange')
-const characterAmountNumber = document.getElementById
-('characterAmountNumber')
-const includeUppercaseElement = document.getElementById
-('includeUppercase')
-const includeNumbers = document.getElementById
-('includeNumbers')
-const includeSymbols = document.getElementById
-('includeSymbols')
-const form = document.getElementById
-('passwordGeneratorForm')
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
 
-const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(65,90)
-const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(97,122)
-const NUMBER_CHAR_CODES = arrayFromLowToHigh(48,57)
-const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33,47).concat (
-    arrayFromLowToHigh(58,64)
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
 ).concat(
-    arrayFromLowToHigh(91,96)  
+  arrayFromLowToHigh(91, 96)
 ).concat(
-    arrayFromLowToHigh(123,126)  
+  arrayFromLowToHigh(123, 126)
+)  
 
 characterAmountNumber.addEventListener('input', syncCharacterAmount)
 characterAmountRange.addEventListener('input', syncCharacterAmount)
@@ -28,8 +24,8 @@ form.addEventListener('submit', e => {
     e.preventDefault()
     const characterAmount = characterAmountNumber.value
     const includeUppercase = includeUppercaseElement.checked
-    const includeNumbers = includeUppercaseElement.checked
-    const includeSymbols = includeUppercaseElement.checked
+    const includeNumbers = includeNumbersElement.checked
+    const includeSymbols = includeSymbolsElement.checked
     const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
 })
 
